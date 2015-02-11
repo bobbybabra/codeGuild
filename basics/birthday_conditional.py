808d4d935f107731fb1fb39679fc1481ddc5af6b
Bobby Babra <corpuscular@hotmail.com>
2015-02-10 19:50:04 -0800

    2nd commit

---
+++ basics/birthday_conditional.py
@@ -0,0 +1,35 @@
+answer = raw_input('  hey dude, what month were you born?: \n use the number of the month: ')
+num = int(answer)
+
+if num == 1:
+	print "\nit's probably cold on your birthday"
+
+elif num == 2:
+	print "\nhope it is not on a leap year!"
+
+elif num == 3:
+	print '\nhope springs eternal... i love march!!'
+
+elif num == 4:
+	print "\nare you an aries?  aren't they stuborn?"
+
+elif num == 5:
+	print '\nget flowers for your birthday, right?'
+
+elif num == 6:
+	print "\nhiya junebug, you're cute"
+
+else:
+	print "\nthat's nice"
+
+color = raw_input('\n  what is your favorite color? ')
+
+if color == 'blue':
+	print "that's my favorite color too!!"
+
+elif color == 'red':
+	print "that's a passionate shade -- yeowzah!"
+
+else:
+	print "okay, whatever, nice chatting with you"
+
