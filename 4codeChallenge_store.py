e56238e40c5832f1c58af9fc48b492a630d1e3ad
Bobby Babra <bobbybabra@Bobbys-iMac.local>
2015-02-25 15:24:20 -0800

    vKev

--- 4codeChallenge_store.py
+++ 4codeChallenge_store.py
@@ -20,90 +20,169 @@ class Wholesale():
 
 class Store(object):
     def __init__(self, item):
-        self.item = widgets
-        self.colors = colors
-        self.size_price = dict(zip('small, medium, large', [10, 20, 30]))
-    def inventory(self):
-        self.items = {}
-
-    def ls_inventory(self):
-        print len(inventory)
+        self.avail_items = []
+        self.customer_basket = []
+        # self.item = widgets
+        # self.colors = colors
+        # self.size_price = dict(zip('small, medium, large', [10, 20, 30]))
 
 
-class Custormer(Store):
+class InventoryItems():
+    def __init__(self, name, on_hand, price):
+        self.name = name
+        self.on_hand = on_hand
+        self.price = price
+
+    def __repr__(self):
+        output = ""
+        output = output + self.name
+        output = output + " @ " + str(self.price)
+        return output
+
+    # def ls_inventory(self):
+    #     print len(inventory)
+
+
+class CartLineItems():
+    def __init__(self, item, quantity):
+        self.item = item
+        self.quantity = quantity
+
+
+class Cart():
     def __init__(self):
-        self.cart = cart
+        self.selected_items = []
 
-class MakeOrder(Wholesale, Store):
-    def placeOrder(self):
-        print ("how many widget do you want to order?")
-        order_input = input(">")
-        if widgets <= 20
-            print "time to order more widgets"
-            print "the store has %d items" % widgets
+    def get_total(self):
+        total = 0.0
+        for line in self.selected_items:
+            line_total = line.quantity * line.item.price
+            total = total + line_total
+        return total
 
-
-class Menu(Store, Customer, Wholesale):
-    def register_menu(self):
-        print "--" * 20a
-        print '--' *20
-        print "enter number for choice"
-        print ("1 Checkout Customer")
-        print ("2 Print Inventory")
-        print ("3 Order from Wholesale")
-        print ("4 Discount Item")
-        print ("5 Search for item")
-        print ("6 Quit")
-        print ("**") * 20
-        print ("**") * 20
-        choice = int(raw_input('> '))
-            if choice == 1:
-                checkout()
-            if choice == 2:
-                ls_inventory()
-            if choice == 3:
-                wholesale_order()
-            if choice == 4:
-                manual_discount()
-            if choice == 5:
-
-            if choice == 6:
-                quit()
-
-class Accounts(Store):
-    def __init__(self):
-        self.name = none
-        self.code = code
-        self.company = company
-        self.money = money
-        self.payable = payable
-        self.expense = expense
-        self.credit = credit
-        self.taxes.taxes
+    def __repr__(self):
+        output = "Cart\n"
+        for line in self.selected_items:
+            output = output + str(line) + "\n"
+        output = output + "\nTotal" + str(self.get_total())
+        return output
 
 
-class Import(Accounts):
-    pass
+class Custormer():
+    def __init__(self, name):
+        self.name = name
+        self.cart = Cart()
 
+# class MakeOrder(Wholesale, Store):
+#     def placeOrder(self):
+#         print ("how many widget do you want to order?")
+#         order_input = input(">")
+#         if widgets <= 20
+#             print "time to order more widgets"
+#             print "the store has %d items" % widgets
+#
+#
+# class Menu(Store, Customer, Wholesale):
+#     def register_menu(self):
+#         print "--" * 20a
+#         print '--' *20
+#         print "enter number for choice"
+#         print ("1 Checkout Customer")
+#         print ("2 Print Inventory")
+#         print ("3 Order from Wholesale")
+#         print ("4 Discount Item")
+#         print ("5 Search for item")
+#         print ("6 Quit")
+#         print ("**") * 20
+#         print ("**") * 20
+#         choice = int(raw_input('> '))
+#             if choice == 1:
+#                 checkout()
+#             if choice == 2:
+#                 ls_inventory()
+#             if choice == 3:
+#                 wholesale_order()
+#             if choice == 4:
+#                 manual_discount()
+#             if choice == 5:
+#
+#             if choice == 6:
+#                 quit()
+#
+# class Accounts(Store):
+#     def __init__(self):
+#         self.name = none
+#         self.code = code
+#         self.company = company
+#         self.money = money
+#         self.payable = payable
+#         self.expense = expense
+#         self.credit = credit
+#         self.taxes.taxes
+#
+#
+# class Import(Accounts):
+#     pass
+#
+#
+# class Export(Accounts):
+#     pass
+#
+#
+# class Checkout(Accounts):
+#     def out(self):
+#         barcode = Accounts.barcode()
+#         register = []
+#
+#
+# class Employees(Accounts):
+#     def __init__(self):
+#         self.ft = fulltime
+#         self.pt = parttime
+#         self.shift = [1, 2, 3]
+#
+#
+# class SaleItems(Checkout):
+#     def __init__(self):
+#         self.specials = specials
 
-class Export(Accounts):
-    pass
+# TEST
+amazon = Store()
 
+amazon.available_items = {
+    111: InventoryItem("Farenheit 451", 10, 4.99),
+    222: InventoryItem("World According to Garp", 5, 4.99),
+    333: InventoryItem("Stranger in a Stange Land.", 1, 4.99),
+}
+amazon.customer_list = {
+    11: Customer("Bob"),
+    22: Customer("Carol"),
+}
 
-class Checkout(Accounts):
-    def out(self):
-        barcode = Accounts.barcode()
-        register = []
+# ##
+# TEST
+# ##
 
+# who are you? select a customer.
+bob = amazon.customer_list[11]
 
-class Employees(Accounts):
-    def __init__(self):
-        self.ft = fulltime
-        self.pt = parttime
-        self.shift = [1, 2, 3]
+# what do you want to buy?
+item = amazon.available_items[333]
 
+#how many?
+qty = 2
 
-class SaleItems(Checkout):
-    def __init__(self):
-        self.specials = specials
+#add to cart
+bob.cart.selected_items.append(CartLineItem(item, qty))
 
+#what do you want to buy?
+item = amazon.available_items[222]
+
+#how many?
+qty = 3
+
+#add to cart
+bob.cart.selected_items.append(CartLineItem(item, qty))
+
+#add more? if no then checkout and print cart
+print bob.cart
